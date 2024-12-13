d9181bd8772dfddc3fe105776270a72d6b62231f
Sanjarbek1024 <gsanjarbek1024@gmail.com>
2024-12-13 15:19:07 +0300

    Lesson 1

---
+++ lesson-2/homework/numeric.py
@@ -0,0 +1,29 @@
+import math # built in library
+# pow=** - built-in function
+# min, max
+
+# int 
+# float
+# complex
+
+# a = 100_000_000_000
+# print(a)
+# print(type(a))
+
+# a1 = "-"
+# print(a1 * 50)
+
+# b = 14e9
+# print(b)
+# print(type(b))
+
+# print(a1 * 50)
+
+# print(math.sqrt(9))
+# print(math.sqrt(16))
+
+
+# Complex
+c = 5 + 4j # 5 c.real 4 c.imag
+print(c.imag % c.real)
+print(c.real // c.imag)
