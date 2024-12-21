7ac55c1650edd0ae3a77cffd3ef43c8a10da403c
Sanjarbek1024 <gsanjarbek1024@gmail.com>
2024-12-21 14:38:38 +0300

    birinchi

---
+++ onlinelesson-2/homework/boolean_intro.py
@@ -0,0 +1,10 @@
+
+# True and False
+
+# >, <, ==, >=, <= !=
+
+a = 8
+b = 8
+print(a == b)
+
+print(bool(0))
---
+++ onlinelesson-2/homework/str_simple_app.py
@@ -0,0 +1,5 @@
+word = "Wordddp"
+
+last_character = word[-1]
+print("last charater is:",last_character, end=" ")
+print("last charater is:",last_character)
